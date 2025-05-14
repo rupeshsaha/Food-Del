9f12db73eb6cd9a27ad22d8581e7a10d46305b18
rupesh <rupeshsaha899@gmail.com>
2025-05-14 08:49:35 +0530

    bug fix

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { menu_list } from "../../assets/assets";
-import "./ExploreMenu.css";
+import "./Exploremenu.css";
 
 const ExploreMenu = ({ category, setCategory }) => {
   return (
